1eed4fe6cc17f9df0d926a691b9fe1fdab8bd9ed
Augusto F. de Abreu <augustoabreu.202@gmail.com>
2019-08-20 19:46:09 -0400

    minhas alteracoes

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,44 @@
   <v-app id="keep">
     <v-app-bar app clipped-left color="amber">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <span class="title ml-3 mr-5">Smart&nbsp;<span class="font-weight-light">Market</span></span>
+      <span class="title ml-3 mr-5">
+        Smart&nbsp;
+        <span class="font-weight-light">Market</span>
+      </span>
 
       <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
-
+      <router-link to="/produtos">Produtos</router-link>
     </v-navigation-drawer>
 
     <v-content>
       <router-view></router-view>
     </v-content>
   </v-app>
-
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-
-    }),
-  }
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
 </script>
 <style>
-  #keep .v-navigation-drawer__border {
-    display: none
-  }
+#keep .v-navigation-drawer__border {
+  display: none;
+}
+a {
+  text-align: center;
+  font-size: 20pt;
+  font-family: sans-serif;
+  font-weight: bold;
+  color: #2c3e50;
+
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Produtos from './views/cadastro-produto.vue'
 
 Vue.use(Router)
 
@@ -11,5 +12,11 @@ export default new Router({
     path: '/',
     name: 'home',
     component: Home
-  }]
+  },
+  {
+    path: '/produtos',
+    name: 'produtos' , 
+    component: Produtos
+  }
+]
 })
