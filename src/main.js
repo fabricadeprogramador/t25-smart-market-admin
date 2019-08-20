9da3938d45c591c89d9b9447c81f967824df8ab5
Rodrigo Reis <rodrigoreis09@gmail.com>
2019-08-20 19:46:25 -0400

    Tela de controle Alef e Rodrigo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import 'vue-material-design-icons/styles.css'
 
 Vue.config.productionTip = false
 
---
+++ src/views/Controle.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-card-title>Produtos com estoque baixo</v-card-title>
+    <v-data-table :headers="headers" :items="produtos" :items-per-page="10" class="elevation-1"></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "Produto", align: "left", sortable: true, value: "name"},
+        { text: "Quantidade", value: "quantidade" },
+        { text: "Validade",align: "right", value: "validade" }
+      ],
+      produtos: [
+        {
+          name: "Guaraná Antártica",
+          quantidade: 159,
+          validade: '01/09/2019'
+        },
+        {
+          name: "Ice cream sandwich",
+          quantidade: 237,
+          validade: '25/09/2019'
+        },
+        {
+          name: "Eclair",
+          quantidade: 262,
+          validade: '12/05/2020'
+        },
+        {
+          name: "Cupcake",
+          quantidade: 305,
+          validade: '25/04/2000'
+        },
+        {
+          name: "Gingerbread",
+          quantidade: 356,
+          validade: '01/09/2018'
+        },
+        {
+          name: "Jelly bean",
+          quantidade: 375,
+          validade: '12/07/2028'
+        },
+        {
+          name: "Lollipop",
+          quantidade: 392,
+          validade: '01/09/2019'
+        },
+        {
+          name: "Honeycomb",
+          quantidade: 408,
+          validade: '30/03/2019'
+        },
+        {
+          name: "Donut",
+          quantidade: 452,
+          validade: '29/11/2037'
+        },
+        {
+          name: "KitKat",
+          quantidade: 518,
+          validade: '05/02/2020'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
