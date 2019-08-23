fa50591911ef63d41a22b21c7dee186c6b5cefd5
felipe valdez <fvaldez018@gmail.com>
2019-08-22 20:34:52 -0400

    subindo clientes

--- src/views/Clientes.vue
+++ src/views/Clientes.vue
@@ -1,102 +1,72 @@
 <template>
-<div class="ma-12">
+  <div class="ma-12">
+    <v-form class="ma-12" ref="form" v-model="valid" lazy-validation>
+      <div class="text-center">
+        <h1>Cadastro de Clientes</h1>
+      </div>
 
-<v-form
-    class="ma-12"
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <div class="text-center">
-       <h1>
-          Cadastro de Clientes
-       </h1>
-    </div>
+      <v-text-field
+        class="mx-10"
+        v-model="name"
+        :counter="30"
+        :rules="nameRules"
+        label="Nome"
+        required
+      ></v-text-field>
 
-    <v-text-field
-      class='mx-10'
-      v-model="name"
-      :counter="30"
-      :rules="nameRules"
-      label="Nome"
-      required
-    ></v-text-field>
+      <v-text-field class="mx-10" v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
 
-    <v-text-field
-      class='mx-10'
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+      <v-text-field
+        class="mx-10"
+        v-model="password"
+        :rules="[rules.required, rules.min]"
+        :type="'password'"
+        name="input-10-1"
+        label="Senha"
+        hint="Use oito ou mais caracteres"
+        counter
+      ></v-text-field>
 
-    <v-text-field
-      class='mx-10'
-      v-model="password"
-      :rules="[rules.required, rules.min]"
-      :type="'password'"
-      name="input-10-1"
-      label="Senha"
-      hint="Use oito ou mais caracteres"
-      counter
-    ></v-text-field>
+      <div class="mx-10">
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="cadastrar">Salvar</v-btn>
 
-    <div
-      class='mx-10'
-    >
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="cadastrar"
-    >
-      Salvar
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="cancelar"
-    >
-      Cancelar
-    </v-btn>
-    </div>
-
-  </v-form>
-</div>
+        <v-btn color="error" class="mr-4" @click="cancelar">Cancelar</v-btn>
+      </div>
+    </v-form>
+  </div>
 </template>
 <script>
 export default {
- data: () => ({
-      valid: true,
-      clientes: [],
-      name: '',
-      nameRules: [
-        v => !!v || 'Nome é obrigatório!',
-        v => (v && v.length <= 30) || 'Nome deve ter no máximo 30 caracteres!',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail é obrigatório!',
-        v => /.+@.+\..+/.test(v) || 'E-mail invalido',
-      ],
-        password: 'Password',
-        rules: {
-          required: value => !!value || 'Senha é obrigatório!.',
-          min: v => v.length >= 8 || 'Min 8 caracteres',
-    },
+  data: () => ({
+    valid: true,
+    clientes: [],
+    name: "",
+    nameRules: [
+      v => !!v || "Nome é obrigatório!",
+      v => (v && v.length <= 30) || "Nome deve ter no máximo 30 caracteres!"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail é obrigatório!",
+      v => /.+@.+\..+/.test(v) || "E-mail invalido"
+    ],
+    password: "Password",
+    rules: {
+      required: value => !!value || "Senha é obrigatório!.",
+      min: v => v.length >= 8 || "Min 8 caracteres"
+    }
+  }),
 
- }),
-
-    methods: {
-      cadastrar () {
-        if (this.$refs.form.cadastrar()) {
-          alert('Usuário cadastrado com sucesso!')
-        }
-      },
-      cancelar () {
-        this.$refs.form.cancelar()
-      },
+  methods: {
+    cadastrar() {
+      alert("ola vue")
+      if (this.$refs.form.cadastrar()) {
+        alert("Usuário cadastrado com sucesso!");
+      }
     },
-}
+    cancelar() {
+      this.$refs.form.cancelar();
+    }
+  }
+};
 </script>
