9a5964cbc59ffed22e4afb2d1ea4cf3a8d778f83
reiscrodrigo <reis.c.rodrigo@gmail.com>
2019-08-29 20:22:12 -0400

    Consumindo API

--- src/views/Controle.vue
+++ src/views/Controle.vue
@@ -1,132 +1,83 @@
 <template>
-<v-card>
   <v-card>
-    <v-card-title>Produtos com estoque baixo</v-card-title>
-    <v-data-table :headers="headers" :items="produtosestoque" :items-per-page="5" class="elevation-1"></v-data-table>
-  </v-card>
+    <v-card>
+      <v-card-title>Produtos com estoque baixo</v-card-title>
+      <v-data-table
+        :headers="headerProdEst"
+        :items="listaProdEst"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card>
 
-  <v-card class="mt-10" :elevation=15>
-    <v-card-title>Produtos com vencimento próximo</v-card-title>
-    <v-data-table :headers="headers" :items="produtosvencimento" :items-per-page="5" class="elevation-1"></v-data-table>
-  </v-card>  
-</v-card>
+    <v-card class="mt-10" :elevation="15">
+      <v-card-title>Produtos com vencimento próximo</v-card-title>
+      <v-data-table
+        :headers="headersProdVencto"
+        :items="listaProdVencto"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card>
+  </v-card>
 </template>
 
 
 <script>
+import HttpRequestUtil from "@/util/HttpRequestUtil";
+
 export default {
   data() {
     return {
-      headers: [
-        { text: "Produto", align: "left", sortable: true, value: "name"},
-        { text: "Quantidade", value: "quantidade" },
-        { text: "Validade",align: "right", value: "validade" }
-      ],
-      produtosestoque: [
+      headerProdEst: [
+        { text: "Produto", align: "left", sortable: true, value: "descricao" },
+        { text: "Marca", align: "left", sortable: true, value: "marca" },
         {
-          name: "Guaraná Antártica Estoque",
-          quantidade: 159,
-          validade: '01/09/2019'
-        },
-        {
-          name: "Ice cream sandwich",
-          quantidade: 237,
-          validade: '25/09/2019'
-        },
-        {
-          name: "Eclair",
-          quantidade: 262,
-          validade: '12/05/2020'
-        },
-        {
-          name: "Cupcake",
-          quantidade: 305,
-          validade: '25/04/2000'
-        },
-        {
-          name: "Gingerbread",
-          quantidade: 356,
-          validade: '01/09/2018'
-        },
-        {
-          name: "Jelly bean",
-          quantidade: 375,
-          validade: '12/07/2028'
-        },
-        {
-          name: "Lollipop",
-          quantidade: 392,
-          validade: '01/09/2019'
-        },
-        {
-          name: "Honeycomb",
-          quantidade: 408,
-          validade: '30/03/2019'
-        },
-        {
-          name: "Donut",
-          quantidade: 452,
-          validade: '29/11/2037'
-        },
-        {
-          name: "KitKat",
-          quantidade: 518,
-          validade: '05/02/2020'
+          text: "Qtde. Disponível",
+          align: "right",
+          sortable: true,
+          value: "qtdeDisponivel"
         }
       ],
-      produtosvencimento: [
+      listaProdEst: [],
+
+      headersProdVencto: [
+        { text: "Produto", align: "left", sortable: true, value: "descricao" },
+        { text: "Marca", align: "left", sortable: true, value: "marca" },
         {
-          name: "Guaraná Antártica Vencto",
-          quantidade: 159,
-          validade: '01/09/2019'
+          text: "Validade",
+          align: "right",
+          sortable: true,
+          value: "validade"
+        }
+      ],
+      listaProdVencto: [
+        {
+          descricao: "Arroz",
+          marca: "Tio João",
+          validade: "11/11/2019"
         },
         {
-          name: "Ice cream sandwich",
-          quantidade: 237,
-          validade: '25/09/2019'
-        },
-        {
-          name: "Eclair",
-          quantidade: 262,
-          validade: '12/05/2020'
-        },
-        {
-          name: "Cupcake",
-          quantidade: 305,
-          validade: '25/04/2000'
-        },
-        {
-          name: "Gingerbread",
-          quantidade: 356,
-          validade: '01/09/2018'
-        },
-        {
-          name: "Jelly bean",
-          quantidade: 375,
-          validade: '12/07/2028'
-        },
-        {
-          name: "Lollipop",
-          quantidade: 392,
-          validade: '01/09/2019'
-        },
-        {
-          name: "Honeycomb",
-          quantidade: 408,
-          validade: '30/03/2019'
-        },
-        {
-          name: "Donut",
-          quantidade: 452,
-          validade: '29/11/2037'
-        },
-        {
-          name: "KitKat",
-          quantidade: 518,
-          validade: '05/02/2020'
+          descricao: "Feijão",
+          marca: "Tio X",
+          validade: "11/09/2019"
         }
       ]
     };
+  },
+  methods: {
+    buscarTodos() {
+      HttpRequestUtil.buscarProdutos().then(produtos => {
+        for (let i = 0; i < produtos.length; i++) {
+          if (parseFloat(produtos[i].qtdeDisponivel) < 50) {
+            this.listaProdEst.push(produtos[i]);
+          }
+        }
+      });
+    }
+  },
+  mounted() {
+    this.buscarTodos();
   }
 };
 </script>
