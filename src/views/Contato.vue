70f31eee07739f7ab96723f4a8937a98b280a4dd
ABabayaga <alef_rdl@hotmail.com>
2019-09-04 13:47:05 -0400

    Alteracao do menu Contato

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,65 +1,140 @@
 <template>
-  <div  class="mx-5 my-10" >
-    <v-dialog
- 
-      v-model="dialog"
-      width="600"
-    >
+  <v-card
+  class="mx-4 my-6 pa-md-2 "
+  
+    max-width="1200"
+    
+  >
+   
+
+    <v-list two-line>
+      <v-list-item-group
+        v-model="selected"
+        multiple
+        active-class="green--text"
+      >
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.title">
+            <template v-slot:default="{ active, toggle }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
+                <v-icon 
+                  
+                  v-if="!active"
+                  color="grey lighten-1"
+                >
+                {{ item.icon }}
+                  star_border
+                </v-icon>
+
+                <v-icon
+                  v-else
+                  color="yellow"
+                >
+                {{ item.icon }}
+                  star
+                </v-icon>
+
+
+<v-row >
+    <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on }">
-        <v-btn
-          v-if="lido"
-          color="red lighten-2"
-          dark
-          width="200"
-          v-on="on"
-        >
-          Cliente X
-        </v-btn>
+        <v-btn  color="primary" dark v-on="on">Responder</v-btn>
       </template>
-
-     
       <v-card>
-        <v-card-title
-        
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Assunto: Sugestão sobre itens no painel.
+        <v-card-title>
+          <span class="headline">User Profile</span>
         </v-card-title>
-
         <v-card-text>
-          Poderia add a opção de detalhes no carrinho de produtos.
+          <v-container>
+            <v-row>
+              <!-- <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-col> -->
+              <!-- <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col> -->
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Assunto" type="password" required></v-text-field>
+              </v-col>
+              <!-- <v-col cols="12" sm="6">
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-col> -->
+            </v-row>
+          </v-container>
+          <!-- <small>*indicates required field</small> -->
         </v-card-text>
-
-        <v-divider></v-divider>
-
         <v-card-actions>
           <div class="flex-grow-1"></div>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            Fechar
-          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Fechar</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Enviar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-  </div>
+  </v-row>
+
+
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider
+            v-if="index + 1 < items.length"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
-export default {
-    data () {
-      return {
-        dialog: false,
-        lido: true,
-      }
-    },
+ 
+ export default {
+    data: () => ({
+      dialog: "false",
+      selected: [2],
+      items: [
+        {
+          icon: "mdi-star",
+          action: '03/09/2019 21:00am',
+          headline: 'Assunto',
+          title: 'Cliente X',
+          subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+        },
+        
+      ],
+    }),
   }
 </script>
-
-<style>
 
 
 
