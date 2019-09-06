3a7627e0ea2cfc655c8c4afa4bf0aeb51f2b7421
reiscrodrigo <reis.c.rodrigo@gmail.com>
2019-09-05 20:02:08 -0400

    Alterações listagem de contatos

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -14,6 +14,7 @@
                   <v-col cols="2" align="right">
                     <v-list-item-action-text v-text="item.datacontato"></v-list-item-action-text>
                     
+                    
                   </v-col>
                 </v-row>
 
