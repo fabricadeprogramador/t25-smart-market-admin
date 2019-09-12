4bc7af79ab6c3fe43fc31f7a1518a5e96ec10a00
Vinicius Souza <viniciuspodi@gmail.com>
2019-09-12 19:45:41 -0400

    Corrigindo conflito de git

--- src/views/Compras.vue
+++ src/views/Compras.vue
@@ -1,47 +1,86 @@
 <template>
-<v-card>
-<v-text-field
-        v-model="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-<v-data-table
-    :headers="cabecalho"
-    :items="produtos"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
-</v-card>
+  <div class="text-center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title>
+          Compras realizadas:
+          <div class="flex-grow-1"></div>
+          <v-text-field
+            v-model="search"
+            label="Procurar produto"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="compras"
+          :search="search"
+        ></v-data-table>
+        <v-col>
+          <v-list-item-title class="title">
+            VALOR TOTAL DAS COMPRAS :
+            <v-list-item-title>R$: {{valortotalDasCompras}}</v-list-item-title>
+          </v-list-item-title>
+        </v-col>
+      </v-card>
+    </v-col>
+  </div>
 </template>
 
 <script>
-import HttpRequestUtil from "@/util/HttpRequestUtil";
+  import HttpRequestUtil from "@/util/HttpRequestUtil";
 
   export default {
-   data () {
+    data() {
       return {
-        cabecalho: [
-          {text: 'Produtos', value: 'name'},
-          { text: 'Valor(R$)', value: 'valorTotal' },
-          { text: 'Data', value: 'data' },
-          { text: 'Pagamento', value: 'pagamento' },
-          { text: 'Cliente', value: 'cliente' },
+        valortotalDasCompras: 0,
+        search: "",
+        headers: [
+          {
+            text: "Produtos",
+            align: "left",
+            sortable: false,
+            value: "produtos"
+          },
+
+          {
+            text: "Valor (R$)",
+            value: "valorTotal"
+          },
+          {
+            text: "Data",
+            value: "data"
+          },
+          {
+            text: "Pagamento",
+            value: "pagamento"
+          },
+          {
+            text: "Cliente",
+            value: "cliente"
+          }
         ],
-        produtos: []
+        compras: []
+      };
+    },
+
+    methods: {
+      buscarCompras() {
+        HttpRequestUtil.buscarCompras().then(response => {
+          this.compras = response;
+
+          for (let i = 0; i < this.compras.length; i++) {
+            this.valortotalDasCompras += parseFloat(this.compras[i].valorTotal);
+          }
+        });
       }
     },
-  methods: {
-    buscarCompras(){
-         HttpRequestUtil.buscarCompras().then(produtos => {
-        this.produtos = produtos;
-      });
+    mounted() {
+      this.buscarCompras();
     }
-  },
-  mounted() {
-    this.buscarCompras()
-  },
-  }
+  };
 </script>
+
 <style>
 </style>
