911c0fb5e321bf418756fb06abc5593c90b732c6
Vinicius Souza <viniciuspodi@gmail.com>
2019-09-19 19:58:12 -0400

    Voltando para versao anterior

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,14 @@
         <h1 class="font-weight-bold display-1 basil--text">Seja bem vindo(a) ao Smart Market, Admin!</h1>
       </v-card-title>
     </v-card>
-    <v-img
-      src="https://www.pinclipart.com/picdir/big/62-622792_flatworld-logo-empty-shopping-cart-transparent-clipart.png"
-      width="500"
-      class="mx-auto"
-    ></v-img>
+    <div class="my-10">
+      <v-img
+        src="https://www.pinclipart.com/picdir/big/62-622792_flatworld-logo-empty-shopping-cart-transparent-clipart.png"
+        width="500" class="mx-auto"></v-img>
+    </div>
   </div>
 </template>
 
 <script>
+  export default {}
 </script>
