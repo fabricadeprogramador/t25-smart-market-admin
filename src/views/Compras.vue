5987b205fce3a4124f0095957c319e47ebc917e1
unknown <vladimirpalermo@hotmail.com>
2019-10-22 19:50:15 -0400

    modificando compras

--- src/views/Compras.vue
+++ src/views/Compras.vue
@@ -12,10 +12,10 @@
           :items="compras"
           item-key="compras.length"
           :search="search"
-          single-expand="true"
+          :single-expand= "singleExpand"
           show-expand
         >
-         <v-btn color="primary" dark v-on="on">Lista de produtos</v-btn>
+         
           <template v-slot:expanded-item="{ headers }">
             <td :colspan="headers.length">{{headers.produtos}}</td>
           </template>
@@ -65,6 +65,7 @@ export default {
       dialogm1: "",
       dialog: false,
       expanded: [],
+      singleExpand: true,
 
       headers: [
         { text: "Detalhes da compra", align: "left" },
