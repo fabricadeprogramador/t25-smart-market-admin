340dc98eac5478bed9807bac6609e268b77ec941
f3l1p3v4 <fvaldez018@gmail.com>
2019-11-29 12:05:51 -0500

    Finalizado Produtos

--- src/views/Usuario.vue
+++ src/views/Usuario.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div>
+      <!-- Alert -->
       <v-snackbar
         type="info"
         v-model="salvo"
@@ -24,13 +25,16 @@
         <v-btn dark text @click="naoCadastrado = false">Fechar</v-btn>
       </v-snackbar>
     </div>
+
+    <!-- Cadastro -->
     <div class="ma-12 elevation-1">
-      <div class="text-center">
+      <div class="text-center pt-4">
         <h1>Cadastro de Usuários</h1>
       </div>
       <v-form v-model="valid">
         <v-container>
           <v-row>
+            <!-- Coluna Username -->
             <v-col cols="12" md="4">
               <v-text-field
                 v-model="username"
@@ -41,6 +45,7 @@
               ></v-text-field>
             </v-col>
 
+            <!-- Coluna Password -->
             <v-col cols="12" md="4">
               <v-text-field
                 v-model="password"
@@ -52,13 +57,15 @@
               ></v-text-field>
             </v-col>
 
+            <!-- Coluna Tipo -->
             <v-col cols="12" md="4">
               <v-select :items="items" label="Tipo" v-model="tipo"></v-select>
             </v-col>
           </v-row>
 
           <div class="text-center">
-            <v-btn color="info" class="my-5" @click="salvar">Salvar</v-btn>
+            <v-btn color="red" class="white--text" @click="limpaCampos">Cancelar</v-btn>
+            <v-btn color="info" class="my-5 ml-5" @click="salvar">Salvar</v-btn>
           </div>
         </v-container>
       </v-form>
@@ -67,53 +74,53 @@
     <!--Lista de Usuarios-->
     <div class="ma-12 elevation-1">
       <v-card>
-        <v-list class="pa-12">
-          <div class="text-center">
-            <h1>Usuários</h1>
-          </div>
-          <v-list-item flat class="title">
-            <v-list-item-title>NOME</v-list-item-title>
+        <div class="text-center mt-4">
+          <h1>Usuários</h1>
+        </div>
+        <v-simple-table light>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-center">NOME</th>
+                <th class="text-center">TIPO</th>
+                <th class="text-center">ATIVAR/INATIVAR</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="usuario in usuarios" :key="usuario._id">
+                <td class="text-center">{{ usuario.username }}</td>
+                <td class="text-center">{{ usuario.tipo }}</td>
+                <td class="text-center">
+                  <v-btn icon @click="mostrarDialog(usuario)">
+                    <v-icon v-if="usuario.ativo" color="green">{{ativado}}</v-icon>
+                    <v-icon v-else color="grey">{{desativado}}</v-icon>
+                  </v-btn>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+        <template>
+          <v-row justify="center">
+            
+            <!-- Dialog de ativação e inativação-->
+            <v-dialog v-model="dialog" max-width="290">
+              <v-card>
+                <v-card-title class="headline">Olá Admin!</v-card-title>
 
-            <v-list-item-title>TIPO</v-list-item-title>
-          </v-list-item>
+                <v-card-text>Deseja realmente alterar status do usuário?</v-card-text>
 
-          <v-list-item v-for="usuario in usuarios" :key="usuario._id">
-            <v-list-item-content>
-              <v-list-item-title v-text="usuario.username"></v-list-item-title>
-            </v-list-item-content>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
 
-            <v-list-item-content>
-              <v-list-item-title v-text="usuario.tipo"></v-list-item-title>
-            </v-list-item-content>
+                  <v-btn color="green darken-1" text @click="dialog = false">Cancelar</v-btn>
 
-            <v-list-item-content class="text-rigth">
-              <v-btn icon @click="mostrarDialog(usuario)">
-                <v-icon v-if="usuario.ativo" color="green">{{ativado}}</v-icon>
-                <v-icon v-else color="grey">{{desativado}}</v-icon>
-              </v-btn>
-            </v-list-item-content>
-
-            <template>
-              <v-row justify="center">
-                <v-dialog v-model="dialog" max-width="290">
-                  <v-card>
-                    <v-card-title class="headline">Olá Admin!</v-card-title>
-
-                    <v-card-text>Deseja realmente alterar status do usuário?</v-card-text>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-
-                      <v-btn color="green darken-1" text @click="dialog = false">Cancelar</v-btn>
-
-                      <v-btn color="green darken-1" text @click="statusUsuario()">Aceitar</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-row>
-            </template>
-          </v-list-item>
-        </v-list>
+                  <v-btn color="green darken-1" text @click="statusUsuario()">Aceitar</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+        </template>
       </v-card>
     </div>
   </div>
@@ -121,22 +128,31 @@
 
 <script>
 import HttpRequestUtil from "@/util/HttpRequestUtil";
-import { ftruncate } from 'fs';
+import { ftruncate } from "fs";
 
 export default {
   data: () => ({
     valid: false,
+    naoCadastrado: false,
+    usuarioStatus: null,
+    salvo: false,
+    dialog: false,
+
+    //Icones
     ativado: "mdi-check-bold",
     desativado: "mdi-cancel",
+
+    //Variáveis do Usuario
     id: 0,
     ativo: true,
     username: "",
     password: "",
-    dialog: false,
-    salvo: false,
-    naoCadastrado: false,
+    tipo: "",
+    items: ["ADMIN", "CLIENTE"],
+
+    //Array de usuarios
+    usuarios: [],
     y: "top",
-    usuarioStatus: null,
     passwordRules: [
       v => !!v || "Senha é obrigatória",
       v => v.length <= 10 || "Senha deve ter no máximo 10 caracteres"
@@ -144,10 +160,7 @@ export default {
     usernameRules: [
       v => !!v || "Username é obrigatório",
       v => v.length <= 10 || "Username deve ter no máximo 10 caracteres"
-    ],
-    tipo: "",
-    items: ["ADMIN", "CLIENTE"],
-    usuarios: []
+    ]
   }),
   methods: {
     salvar() {
@@ -163,7 +176,7 @@ export default {
         HttpRequestUtil.salvarUsuario(usuario).then(response => {
           this.usuarios.push(response);
           this.salvo = true;
-          this.limpaCampos()
+          this.limpaCampos();
         });
       } else {
         this.naoCadastrado = true;
@@ -179,9 +192,9 @@ export default {
     },
 
     limpaCampos() {
-      this.username = '';
-      this.password = '';
-      this.tipo = '';
+      this.username = "";
+      this.password = "";
+      this.tipo = "";
     },
 
     buscarTodos() {
